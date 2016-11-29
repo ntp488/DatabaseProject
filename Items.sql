8941cf07f195b42c01744f26d4651ab78b3aa5cd
ntp488 <ntp488@gmail.com>
2016-11-29 17:37:01 -0600

    Adds basic items.sql

---
+++ Items.sql
@@ -0,0 +1,124 @@
+/*
+Arches
+*/
+
+INSERT INTO Items (ItemName, ItemStock)
+VALUES
+(
+    'Coral Coast Arch',
+    5,
+);
+
+INSERT INTO Items (ItemName, ItemStock)
+VALUES
+(
+    'White Rose Metal Arch',
+    10,
+);
+
+INSERT INTO Items (ItemName, ItemStock)
+VALUES
+(
+    'Birch Pole Arch',
+    3,
+);
+
+/*
+Cake serving set
+*/
+
+INSERT INTO Items (ItemName, ItemStock)
+VALUES
+(
+    'Lacy Crystal Serving Set',
+    10,
+);
+
+INSERT INTO Items (ItemName, ItemStock)
+VALUES
+(
+    'Personalized Silver Serving Set',
+    3,
+);
+
+INSERT INTO Items (ItemName, ItemStock)
+VALUES
+(
+    'Personalized Serving Set',
+    3,
+);
+
+/*
+Aisle Runners
+*/
+
+INSERT INTO Items (ItemName, ItemStock)
+VALUES
+(
+    'Always And Forever Aisle Runner',
+    6,
+);
+
+INSERT INTO Items (ItemName, ItemStock)
+VALUES
+(
+    'Two Become One Aisle Runner',
+    4,
+);
+
+INSERT INTO Items (ItemName, ItemStock)
+VALUES
+(
+    'Lace And Rayon Aisle Runenr',
+    3,
+);
+
+/*
+Flowers
+*/
+
+INSERT INTO Items (ItemName, ItemStock)
+VALUES
+(
+    '100 Ct. Peruvian Lily',
+    23,
+);
+
+INSERT INTO Items (ItemName, ItemStock)
+VALUES
+(
+    '12 Ct. Red Roses',
+    32,
+);
+
+INSERT INTO Items (ItemName, ItemStock)
+VALUES
+(
+    '12 Ct. Multicolor Roses',
+    27,
+);
+
+/*
+Dresses
+*/
+
+INSERT INTO Items (ItemName, ItemStock)
+VALUES
+(
+    'Trumpet Mermaid Bead/Lace Dress with Train',
+    2,
+);
+
+INSERT INTO Items (ItemName, ItemStock)
+VALUES
+(
+    'Gauze Scoop Mermaid with Train',
+    2,
+);
+
+INSERT INTO Items (ItemName, ItemStock)
+VALUES
+(
+    'Vera Wang Embroidered Wedding Gown',
+    1,
+);
