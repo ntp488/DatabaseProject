e67eb8c527d49e29c56709a5d233d08adb592332
Kane Stapler <kanestapler@gmail.com>
2016-12-06 17:18:04 -0600

    Fixing sql inserts

--- DatabaseGUI/WeddingSupplies.sql
+++ DatabaseGUI/WeddingSupplies.sql
@@ -1,15 +1,15 @@
-INSERT INTO WeddingSupplies (WeddingID, VendorSupplyID, Quantity)
+INSERT INTO WeddingSupplies (WeddingSuppliesID, WeddingID, VendorSupplyID, Quantity)
 VALUES
-    (0, 8, 1),
-    (0, 12, 2),
-    (0, 1, 1),
-    (1, 2, 2),
-    (2, 5, 1),
-    (2, 13, 2),
-    (2, 7, 1),
-    (2, 20, 2),
-    (2, 24, 1),
-    (3, 27, 2),
-    (3, 30, 25),
-    (3, 33, 1),
-    (3, 18, 3);
+    (0, 0, 8, 1),
+    (1, 0, 12, 2),
+    (2, 0, 1, 1),
+    (3, 1, 2, 2),
+    (4, 2, 5, 1),
+    (5, 2, 13, 2),
+    (6, 2, 7, 1),
+    (7, 2, 20, 2),
+    (8, 2, 24, 1),
+    (9, 3, 27, 2),
+    (10, 3, 30, 25),
+    (11, 3, 33, 1),
+    (12, 3, 18, 3);
--- DatabaseGUI/Weddings.sql
+++ DatabaseGUI/Weddings.sql
@@ -1,6 +1,6 @@
-INSERT INTO Weddings (WeddingDate, TotalPrice, Paid, CustomerID)
+INSERT INTO Weddings (WeddingID, WeddingDate, TotalPrice, Paid, CustomerID)
 VALUES
-    ('2016-12-25', 10500, 'True',5),
-    ('2016-12-31', 1000, 'False',4),
-    ('2017-05-15', 12000, 'True',1),
-    ('2017-07-01', 5600, 'False',6);
+    (0, '2016-12-25', 10500, 'True',5),
+    (1, '2016-12-31', 1000, 'False',4),
+    (2, '2017-05-15', 12000, 'True',1),
+    (3, '2017-07-01', 5600, 'False',6);
